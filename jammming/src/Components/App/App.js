cbf1c7d5079c85f635e3aec9d8e7dc94e8edf629
60162973+abgelvin@users.noreply.github.com <abgelvin@gmail.com>
2022-03-23 16:28:38 -0600

    added App and Component directories and directory for each component

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
+  render(
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>
+    )
+    
+  )
   return (
     <div className="App">
       <header className="App-header">
